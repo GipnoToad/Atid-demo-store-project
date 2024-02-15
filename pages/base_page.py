8e80c6e1f38aa932c36ec123be2bf8be9629bd53
GipnoToad <rskeisik@gmail.com>
2024-02-15 20:57:35 +0200

    Corrections in home_page.py and base_page.py methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common import exceptions
 
 class BasePage:
     def __init__(self, browser: Chrome, url: str):
@@ -27,5 +28,14 @@ class BasePage:
     def element_is_visible(self, locator: tuple, wait=5):
         try:
             WebDriverWait(self.browser, wait).until(EC.visibility_of_element_located(locator))
+            return True
         except:
             raise AssertionError(f'Element: {locator} is not visible.')
+
+    def text_as_expected_text(self, locator:tuple, text: str):
+        try:
+            elem = self.browser.find_element(*locator)
+            assert elem.text == text, f"Expected text: {text}. Actual text: {elem.text}"
+        except exceptions.NoSuchElementException:
+            raise AssertionError(f'Element: {locator} is not readable.')
+
--- pages/home_page.py
+++ pages/home_page.py
@@ -1,6 +1,8 @@
-from _locators import HomePageLocators
-from base_page import BasePage
+from ._locators import HomePageLocators
+from .base_page import BasePage
 
 class HomePage(BasePage):
     def is_home_page(self):
         self.element_is_visible(HomePageLocators.HOME_PAGE_TITLE)
+        self.text_as_expected_text(HomePageLocators.HOME_PAGE_TITLE, "ATID Demo Store")
+        
