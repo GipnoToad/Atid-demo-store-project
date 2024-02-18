7d1fe5e867e9ec4c881b1b93b01ea3c5ea280b90
GipnoToad <rskeisik@gmail.com>
2024-02-18 21:38:44 +0200

    Rewrite some click method

--- pages/base_page.py
+++ pages/base_page.py
@@ -13,17 +13,19 @@ class BasePage:
 
     def element_is_clickable(self, locator: tuple, wait=5):
         try:
-            WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable(locator))
-            return True
-        except:
+            elem = WebDriverWait(self.browser, wait).until(EC.element_to_be_clickable(locator))
+            elem.click()
+        except exceptions.TimeoutException:
             raise AssertionError(f'Element: {locator} is not clickable.')
+        except exceptions.ElementClickInterceptedException as click:
+            raise AssertionError(f"Element: {locator} was not cliked due {click}")
 
-    def click_element(self, locator: tuple):
-        try:
-            self.element_is_clickable(locator)
-            self.browser.find_element(*locator).click()
-        except:
-            raise AssertionError(f'Failed to click element: {locator}.')
+    # def click_element(self, locator: tuple):
+    #     try:
+    #         elem = self.element_is_clickable(locator)
+    #         elem.click()
+    #     except:
+    #         raise AssertionError(f'Failed to click element: {locator}.')
 
     def element_is_visible(self, locator: tuple, wait=5):
         try:
@@ -37,5 +39,5 @@ class BasePage:
             elem = self.browser.find_element(*locator)
             assert elem.text == text, f"Expected text: {text}. Actual text: {elem.text}"
         except exceptions.NoSuchElementException:
-            raise AssertionError(f'Element: {locator} is not readable.')
+            raise AssertionError(f'Element: {locator} is not found.')
 
