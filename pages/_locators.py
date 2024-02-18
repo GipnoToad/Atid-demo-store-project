689e280c411c8a165f568a58beeedca33df1a13b
GipnoToad <rskeisik@gmail.com>
2024-02-18 21:38:17 +0200

    Add store page locators

--- pages/_locators.py
+++ pages/_locators.py
@@ -2,3 +2,7 @@ from selenium.webdriver.common.by import By
 
 class HomePageLocators:
     HOME_PAGE_TITLE = (By.CSS_SELECTOR, "h1.elementor-heading-title")
+    STORE_PAGE_BUTTON = (By.CSS_SELECTOR, "#menu-item-45 a")
+
+class StorePageLocators:
+    CATEGORIES_TITLE = (By.CSS_SELECTOR, "#woocommerce_product_categories-2 h2")
