b2d0d86a031291ff42f53fc5f880fbc13ea1797c
GipnoToad <rskeisik@gmail.com>
2024-02-18 21:39:41 +0200

    Add click_store_page_button method

--- pages/home_page.py
+++ pages/home_page.py
@@ -6,3 +6,8 @@ class HomePage(BasePage):
         self.element_is_visible(HomePageLocators.HOME_PAGE_TITLE)
         self.text_as_expected_text(HomePageLocators.HOME_PAGE_TITLE, "ATID Demo Store")
         
+    def click_store_page_button(self):
+        self.is_home_page()
+        self.element_is_clickable(HomePageLocators.STORE_PAGE_BUTTON)
+
+
