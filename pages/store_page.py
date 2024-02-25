8abafb0928aed7c46cad3944d5efca122f78f2b1
GipnoToad <rskeisik@gmail.com>
2024-02-25 20:04:51 +0200

    Add choose_product method

--- pages/store_page.py
+++ pages/store_page.py
@@ -5,3 +5,7 @@ class StorePage(BasePage):
     def is_store_page(self):
         self.element_is_visible(StorePageLocators.CATEGORIES_TITLE)
         self.text_as_expected_text(StorePageLocators.CATEGORIES_TITLE, "Categories")
+
+    def choose_product(self):
+        self.element_is_visible(StorePageLocators.PRODUCTS_TABLE)
+        self.elements_are_clickable(StorePageLocators.PRODUCTS_TABLE)
