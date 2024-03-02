340de2125614d256e293300a9bc604dbf6f32ed5
GipnoToad <rskeisik@gmail.com>
2024-03-02 10:02:01 +0200

    Fix choose_products method

--- pages/store_page.py
+++ pages/store_page.py
@@ -6,6 +6,6 @@ class StorePage(BasePage):
         self.element_is_visible(StorePageLocators.CATEGORIES_TITLE)
         self.text_as_expected_text(StorePageLocators.CATEGORIES_TITLE, "Categories")
 
-    def choose_product(self):
+    def choose_products(self):
         self.element_is_visible(StorePageLocators.PRODUCTS_TABLE)
-        self.elements_are_clickable(StorePageLocators.PRODUCTS_TABLE)
+        self.click_all_elements_in_list(StorePageLocators.PRODUCTS_TABLE)
