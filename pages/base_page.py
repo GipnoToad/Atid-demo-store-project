fcc4c531f32efaac4d97f87f513779ae7135d225
GipnoToad <rskeisik@gmail.com>
2024-03-02 10:00:49 +0200

    Rewrite method to click all elements

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,12 +2,15 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common import exceptions
-from random import choice
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
 
 class BasePage:
     def __init__(self, browser: Chrome, url: str):
         self.browser = browser
         self.url = url
+        self.actions = ActionChains(self.browser)
 
     def open(self):
         self.browser.get(self.url)
@@ -21,11 +24,13 @@ class BasePage:
         except exceptions.ElementClickInterceptedException as ex:
             raise AssertionError(f"Element: {locator} was not cliked due {ex}")
 
-    def elements_are_clickable(self, locator: tuple, wait=5):
+    def click_all_elements_in_list(self, locator: tuple, wait=5):
             try:
                 WebDriverWait(self.browser, wait).until(EC.visibility_of_element_located(locator))
                 elems = self.browser.find_elements(*locator)
-                choice(elems).click()
+                for elem in elems:
+                    self.actions.key_down(Keys.CONTROL).click(elem).key_up(Keys.CONTROL).perform()
+                    time.sleep(2)
             except exceptions.TimeoutException:
                 raise AssertionError(f'Element: {locator} is not clickable.')
             except exceptions.ElementClickInterceptedException as ex:
