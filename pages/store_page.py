506675671a76f79a0744d2e52fe6da90c5f27cd6
GipnoToad <rskeisik@gmail.com>
2024-03-03 22:45:33 +0200

    Rewrite choose_products, add choose_product method

--- pages/store_page.py
+++ pages/store_page.py
@@ -5,7 +5,14 @@ class StorePage(BasePage):
     def is_store_page(self):
         self.element_is_visible(StorePageLocators.CATEGORIES_TITLE)
         self.text_as_expected_text(StorePageLocators.CATEGORIES_TITLE, "Categories")
+        self.element_is_clickable(StorePageLocators.CATEGORIES_TITLE)
 
     def choose_products(self):
-        self.element_is_visible(StorePageLocators.PRODUCTS_TABLE)
+        self.element_is_clickable(StorePageLocators.PRODUCTS_TABLE)
         self.click_all_elements_in_list(StorePageLocators.PRODUCTS_TABLE)
+
+    def choose_product(self):
+        self.element_is_clickable(StorePageLocators.PRODUCT)
+        product_name = self.get_element_name(StorePageLocators.PRODUCT_NAME_IN_CATALOG)
+        self.click_element(StorePageLocators.PRODUCT)
+        return product_name
