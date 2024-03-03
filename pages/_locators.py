1656fb867dff8096f7f3e1584375ecb46f991c57
GipnoToad <rskeisik@gmail.com>
2024-03-03 22:40:18 +0200

    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_title") Add PRODUCT, PRODUCT_NAME_IN_CATALOG to StorePageLocators, ADD_TO_CART, PRODUCT_NAME to ProductPageLocators

--- pages/_locators.py
+++ pages/_locators.py
@@ -7,4 +7,9 @@ class HomePageLocators:
 class StorePageLocators:
     CATEGORIES_TITLE = (By.CSS_SELECTOR, "#woocommerce_product_categories-2 h2")
     PRODUCTS_TABLE = (By.CSS_SELECTOR, ".astra-shop-thumbnail-wrap")
+    PRODUCT = (By.CSS_SELECTOR, ".products li:nth-child(1)")
+    PRODUCT_NAME_IN_CATALOG = (By.CSS_SELECTOR, ".products li:nth-child(1) h2")
     
+class ProductPageLocators:
+    ADD_TO_CART = (By.CSS_SELECTOR, '[name="add-to-cart"]')
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_title")
